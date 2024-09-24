82653f2e20e6c1a8ad03c2284558d2f3cd4e1956
Halcon <53305213+ghostrecom@users.noreply.github.com>
2024-09-24 21:52:10 +0200

    Add files via upload

--- config.lua
+++ config.lua
@@ -4,6 +4,7 @@ Config.Text = {
     [1] = "There are online doctors, you can't use this.",
     [2] = "The player is not dead",
     [3] = "NPC DOCTOR USED",
+    [4] = "CD espera",
 
 }
 Config.webHook =""
